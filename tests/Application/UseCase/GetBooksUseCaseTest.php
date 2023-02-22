bec9f8c0dddc3b4f674f491c69933fa9c488df09
Garzolio <miguelvalla4@gmail.com>
2023-02-22 20:53:50 +0100

    test-add-initializate-tests-structure

---
+++ tests/Application/UseCase/GetBooksUseCaseTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Application\UseCase;
+
+use PHPUnit\Framework\TestCase;
+
+class GetBookUseCaseTest extends TestCase
+{
+    /** @test */
+    public function getBooks(): void
+    {
+    }
+}
