b6361fbf9a00a7d94726d402254c1effa907df0d
Garzolio <miguelvalla4@gmail.com>
2023-02-25 20:00:24 +0100

    feat/add-new-endpoint-for-list-all-books

--- app/routes.php
+++ app/routes.php
@@ -2,12 +2,10 @@
 
 declare(strict_types=1);
 
-use App\Application\Actions\User\ListUsersAction;
-use App\Application\Actions\User\ViewUserAction;
+use App\Application\Actions\BooksAction;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\App;
-use Slim\Interfaces\RouteCollectorProxyInterface as Group;
 
 return function (App $app) {
     $app->options('/{routes:.*}', function (Request $request, Response $response) {
@@ -19,9 +17,6 @@ return function (App $app) {
         $response->getBody()->write('Hello world!');
         return $response;
     });
-
-    $app->group('/users', function (Group $group) {
-        $group->get('', ListUsersAction::class);
-        $group->get('/{id}', ViewUserAction::class);
-    });
+    
+    $app->get('/books', BooksAction::class);
 };
