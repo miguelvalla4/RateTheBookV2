a83a06e4a6b2ff16fa6663a6deab570c72e2a8b4
Garzolio <miguelvalla4@gmail.com>
2023-03-01 13:39:03 +0100

    refactor/apply-php8-nomenclature

--- src/Infrastructure/Factory/AuthorFactory.php
+++ src/Infrastructure/Factory/AuthorFactory.php
@@ -1,1 +1,20 @@
 <?php
+
+declare(strict_types=1);
+
+namespace App\Infrastructure\Factory;
+
+use App\Domain\Author\Author;
+
+class AuthorFactory
+{
+    public static function buildFromArray(array $primitiveAuthor): Author
+    {
+        return new Author(
+            (int) $primitiveAuthor['id'],
+            (string) $primitiveAuthor['name'],
+            (string) $primitiveAuthor['lastName'],
+            (string) $primitiveAuthor['nationality']
+        );
+    }
+}
