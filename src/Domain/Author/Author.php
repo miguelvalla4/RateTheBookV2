54cb492265f3bf7ebc30a2bae6b75474ea8a904d
Garzolio <miguelvalla4@gmail.com>
2023-03-01 14:24:20 +0100

    feat/add-entity-author-on-domain

--- src/Domain/Author/Author.php
+++ src/Domain/Author/Author.php
@@ -4,10 +4,50 @@ declare(strict_types=1);
 
 namespace App\Domain\Author;
 
-class Author
+use JsonSerializable;
+
+class Author implements JsonSerializable
 {
-    public function __construct()
+    protected int $id;
+    protected string $name;
+    protected string $lastName;
+    protected string $nationality;
+
+    public function __construct(int $id, string $name, string $lastName, string $nationality)
     {
-        
+        $this->id = $id;
+        $this->name = $name;
+        $this->lastName = $lastName;
+        $this->nationality = $nationality;
+    }
+
+    public function id(): int
+    {
+        return $this->id;
+    }
+
+    public function name(): string
+    {
+        return $this->name;
+    }
+
+    public function lastName(): string
+    {
+        return $this->lastName;
+    }
+
+    public function nationality(): string
+    {
+        return $this->nationality;
+    }
+
+    public function jsonSerialize(): array
+    {
+        return [
+            "id" => $this->id(),
+            "name" => $this->name(),
+            "lastName" => $this->lastName(),
+            "nationality" => $this->nationality()
+        ];
     }
 }
