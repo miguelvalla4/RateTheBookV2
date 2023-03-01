7f760bb62555459f98894aae6e04f3f83fa6e1e4
Garzolio <miguelvalla4@gmail.com>
2023-03-01 14:30:14 +0100

    feat/add-new-endpoint-to-list-all-authors

--- app/repositories.php
+++ app/repositories.php
@@ -2,21 +2,24 @@
 
 declare(strict_types=1);
 
+use App\Domain\Author\AuthorRepositoryInterface;
 use App\Domain\Book\BookRepositoryInterface;
+use App\Infrastructure\Pdo\MySql\Author\MySqlAuthorRepository;
 use App\Infrastructure\Pdo\MySql\Book\MySqlBookRepository;
 use DI\ContainerBuilder;
 use Psr\Container\ContainerInterface;
 
 return static function (ContainerBuilder $containerBuilder) {
     // Here we map our UserRepository interface to its in memory implementation
-    try {
         $containerBuilder->addDefinitions([
             BookRepositoryInterface::class => function (ContainerInterface $c) {
                 return new MySqlBookRepository($c->get(PDO::class));
             }
         ]);
-    } catch (Exception $e) {
-        return $e->getMessage();
-    }
 
+        $containerBuilder->addDefinitions([
+            AuthorRepositoryInterface::class => function (ContainerInterface $c) {
+                return new MySqlAuthorRepository($c->get(PDO::class));
+            }
+        ]);
 };
--- app/routes.php
+++ app/routes.php
@@ -2,6 +2,7 @@
 
 declare(strict_types=1);
 
+use App\Application\Actions\AuthorsAction;
 use App\Application\Actions\BooksAction;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
@@ -19,4 +20,5 @@ return function (App $app) {
     });
 
     $app->get('/books', BooksAction::class);
+    $app->get('/authors', AuthorsAction::class);
 };
