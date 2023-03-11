cc8b3365938b0329aec4d5777a524b7ed7cd4fe4
Garzolio <miguelvalla4@gmail.com>
2023-03-11 18:05:41 +0100

    feat/add new endpoint declaration

--- app/routes.php
+++ app/routes.php
@@ -5,9 +5,11 @@ declare(strict_types=1);
 use App\Application\Actions\AuthorsAction;
 use App\Application\Actions\BooksAction;
 use App\Application\Actions\BooksBySagaAction;
+use App\Application\Actions\SaveAuthorAction;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\App;
+use Slim\Interfaces\RouteCollectorProxyInterface as Group;
 
 return function (App $app) {
     $app->options('/{routes:.*}', function (Request $request, Response $response) {
@@ -20,7 +22,13 @@ return function (App $app) {
         return $response;
     });
 
-    $app->get('/books', BooksAction::class);
-    $app->get('/books{saga}', BooksBySagaAction::class);
-    $app->get('/authors', AuthorsAction::class);
+    $app->group('/books', function (Group $group) {
+        $group->get('', BooksAction::class);
+        $group->get('{saga}', BooksBySagaAction::class);
+    });
+
+    $app->group('/author', function (Group $group) {
+        $group->get('', AuthorsAction::class);
+        $group->post('', SaveAuthorAction::class);
+    });
 };
